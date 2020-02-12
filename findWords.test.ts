d9f8d43c7992e8e23080f6ce28a9bf4a3962b8b9
clark-lindsay <clarklindsay96@gmail.com>
2020-02-12 16:24:14 -0500

    will now favor the larger words when given targets are substrings of other targets; e.g. 'word' and 'or'

--- findWords.test.ts
+++ findWords.test.ts
@@ -7,4 +7,18 @@ describe('the findWords function', () => {
 
     expect(findWords(targets, puzzleText)).toEqual({ word: [10, 11, 12, 13], string: [0, 1, 2, 3, 4, 5] });
   });
+
+  it('will favor the longer words when target strings have overlapping indices in the puzzle text', () => {
+    const targets = ['word', 'or'];
+    const puzzleText = 'stringwithwordinit';
+
+    expect(findWords(targets, puzzleText)).toEqual({ word: [10, 11, 12, 13] });
+  });
+
+  it('will throw an error if the puzzleText includes the pipe character ( | ) since this is used in internal calculations', () => {
+    const targets = ['word', 'or'];
+    const puzzleText = 'stringwith|pipes|intit';
+
+    expect(() => findWords(targets, puzzleText)).toThrow();
+  });
 });
--- findWords.ts
+++ findWords.ts
@@ -1,11 +1,19 @@
 import { range } from './util';
 
 export function findWords(targets: string[], puzzleText: string): object {
+  if (puzzleText.includes('|')) {
+    throw new Error('The puzzleText must not include the "pipe" ( | ) character');
+  }
   const result = {};
-  for (const word of targets) {
-    if (puzzleText.includes(word)) {
-      const wordStartIndex = puzzleText.search(word);
+  let mutablePuzzleText = puzzleText;
+  for (const word of targets.sort((a, b) => b.length - a.length)) {
+    if (mutablePuzzleText.includes(word)) {
+      const wordStartIndex = mutablePuzzleText.search(word);
       result[word] = range(wordStartIndex, wordStartIndex + word.length);
+      mutablePuzzleText =
+        mutablePuzzleText.substring(0, wordStartIndex) +
+        '|'.repeat(word.length) +
+        mutablePuzzleText.substring(wordStartIndex + word.length);
     }
   }
   return result;
