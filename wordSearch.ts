29493ec44c2f216363dfa46e1b6834d0fa31b447
clark-lindsay <clarklindsay96@gmail.com>
2020-02-13 16:59:29 -0500

    mapping the coordinates returned to match the (x, y) format expected, and extracted a function for searching all the rows

--- wordSearch.ts
+++ wordSearch.ts
@@ -2,11 +2,18 @@ import { findWords } from './findWords';
 
 export function wordSearch(targets: string[], puzzleGrid: string[]): { [key: string]: number[][] } {
   let result: { [key: string]: number[][] } = {};
-  for (const row of puzzleGrid) {
-    const match = findWords(targets, row.split(',').join(''));
-    for (const [word, indexRange] of Object.entries(match)) {
-      result[word] = indexRange.map(num => [0, num]);
+  searchRows();
+  return result;
+
+  function searchRows() {
+    for (const [index, row] of puzzleGrid.entries()) {
+      const match = findWords(targets, row.split(',').join(''));
+      for (const [word, indexRange] of Object.entries(match)) {
+        result[word] = indexRange
+          .map(xCoordinate => [xCoordinate, index])
+          .sort()
+          .reverse();
+      }
     }
   }
-  return result;
 }
