c3da3f6ff609ebf92801fc8e6a027a2b5b491a5b
clark-lindsay <clarklindsay96@gmail.com>
2020-02-13 11:57:46 -0500

    increasing the specificity of the language of the tests, and added a test to document 'no-result' behavior"

--- findWords.test.ts
+++ findWords.test.ts
@@ -1,11 +1,18 @@
 import { findWords } from './findWords';
 
 describe('the findWords function', () => {
-  it('will return the index ranges of the target strings if it is contained in the input', () => {
+  it('will return the index ranges of the target strings if it is contained, verbatim, in the input', () => {
     const targets = ['word', 'string'];
     const puzzleText = 'stringwithwordinit';
 
     expect(findWords(targets, puzzleText)).toEqual({ word: [10, 11, 12, 13], string: [0, 1, 2, 3, 4, 5] });
+  });
+
+  it('will return an empty object if none of the targets appear in the puzzle text', () => {
+    const targets = ['word', 'string'];
+    const puzzleText = 'totallydifferenttext';
+
+    expect(findWords(targets, puzzleText)).toEqual({});
   });
 
   it('will favor the longer words when target strings have overlapping indices in the puzzle text', () => {
