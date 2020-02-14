96a243f788ea186485f9e253ed40bfd4b5dd272f
clark-lindsay <clarklindsay96@gmail.com>
2020-02-13 19:27:23 -0500

    UNFINISHED/WIP: New idea on how to handle diagonals; by flipping the board. Haven't worked out how to get mirror indices back to normal

--- wordSearch.ts
+++ wordSearch.ts
@@ -1,12 +1,14 @@
 import { findWords } from './findWords';
+import { reverseOf } from './util';
 
 export function wordSearch(targets: string[], puzzleGrid: string[]): { [key: string]: number[][] } {
   let result: { [key: string]: number[][] } = {};
   searchRows();
   searchColumns();
+  searchDiagonals();
   return result;
 
-  function searchRows() {
+  function searchRows(): void {
     for (const [index, row] of puzzleGrid.entries()) {
       const match = findWords(targets, row.split(',').join(''));
       for (const [word, indexRange] of Object.entries(match)) {
@@ -14,9 +16,8 @@ export function wordSearch(targets: string[], puzzleGrid: string[]): { [key: str
       }
     }
   }
-  function searchColumns() {
-    const columns = getColumns();
-    for (const [index, col] of columns.entries()) {
+  function searchColumns(): void {
+    for (const [index, col] of getColumns().entries()) {
       const match = findWords(targets, col.split(',').join(''));
       for (const [word, indexRange] of Object.entries(match)) {
         result[word] = indexRange.map(yCoordinate => [index, yCoordinate]);
@@ -31,6 +32,34 @@ export function wordSearch(targets: string[], puzzleGrid: string[]): { [key: str
         }
       }
       return columns;
+    }
+  }
+
+  function searchDiagonals(): void {
+    for (const [initialXCoordinate, diagonal] of getDiagonals().entries()) {
+      const match = findWords(targets, diagonal.split(',').join(''));
+      for (const [word, indexRange] of Object.entries(match)) {
+        result[word] = indexRange.map(indexInDiag => [initialXCoordinate - indexInDiag, indexInDiag]);
+      }
+    }
+
+    function getDiagonals(): string[] {
+      let diagonals = Array(puzzleGrid.length * 2).fill('');
+      for (const [xCoordinate, row] of puzzleGrid.entries()) {
+        for (const [yCoordinate, char] of row.split(',').entries()) {
+          diagonals[xCoordinate + yCoordinate] += char;
+        }
+      }
+
+      let mirroredDiagonals = Array(puzzleGrid.length * 2).fill('');
+      const mirroredPuzzleGrid = puzzleGrid.map(str => reverseOf(str));
+      for (const [xCoordinate, row] of mirroredPuzzleGrid.entries()) {
+        for (const [yCoordinate, char] of row.split(',').entries()) {
+          mirroredDiagonals[xCoordinate + yCoordinate] += char;
+        }
+      }
+
+      return diagonals.concat(mirroredDiagonals);
     }
   }
 }
